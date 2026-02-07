208deac006516c58eff705f743819eed49afadd0
Surya Raju <narayana573@gmail.com>
2026-02-07 15:48:44 +0530

    Code Updated

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,12 +1,17 @@
 const { test, expect } = require('@playwright/test');
 
 
+
+// https://formy-project.herokuapp.com/
+// https://the-internet.herokuapp.com/
+
+
 test('should have the correct title', async ({ page }) => {
-  await page.goto('https://example.com');
+  await page.goto('https://formy-project.herokuapp.com/');
   await expect(page).toHaveTitle('Example Domain');
 });
 
-test.only('First Test', async ({ page }) => {
+test('First Test', async ({ page }) => {
 
   await page.goto('https://google.com');
 
@@ -15,5 +20,23 @@ test.only('First Test', async ({ page }) => {
   console.log(await page.title());
   await page.locator("textarea#APjFqb").fill("Playwright");
   await page.locator("textarea#APjFqb").press('Enter');
+
+});
+
+test.only('Pratice all the elements Test', async ({ page }) => {
+
+  await page.goto('https://formy-project.herokuapp.com');
+
+  await page.screenshot({ path: 'screenshot/screenshot.png' });
+
+  
+  console.log(await page.title());
+  await page.locator("//*[contains(text(),'Complete Web Form') and @class='btn btn-lg']").click();
+  await page.screenshot({ path: 'screenshot/screenshot1.png' });
+  
+  console.log(await page.title());
+  await page.locator("#first-name").fill("John");
+  await page.waitForTimeout(9000);
+    
 
 });
